972fbbd9e38849172126790b3de40fa442ff90a9
greheine <greheine@baec0e06-7d18-0410-8a00-dfbcef9d3b84>
2006-10-18 16:44:33 +0000

    Extracted inner class ScriptLauncher and added javadoc reporting to pom.

--- src/main/java/org/codehaus/xharness/tasks/XhExecBgTask.java
+++ src/main/java/org/codehaus/xharness/tasks/XhExecBgTask.java
@@ -17,32 +17,23 @@
 
 package org.codehaus.xharness.tasks;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketTimeoutException;
 import java.util.Iterator;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
-import org.apache.tools.ant.Task;
 import org.apache.tools.ant.taskdefs.Sequential;
 import org.apache.tools.ant.types.Commandline;
 
 import org.codehaus.xharness.procutil.BgProcess;
 import org.codehaus.xharness.procutil.ProcessRegistry;
+import org.codehaus.xharness.procutil.ScriptLauncher;
 
 /**
  * @author Gregor Heine
@@ -60,7 +51,7 @@ public class XhExecBgTask extends XhExecTask implements Runnable, BgProcess {
     private Sequential afterwards;
     private File launcherArgsFile = null;
     private String launchedCommandLine = null;
-    private Launcher launcher = null;
+    private ScriptLauncher launcher = null;
 
     /**
      * Constructor.
@@ -110,7 +101,7 @@ public class XhExecBgTask extends XhExecTask implements Runnable, BgProcess {
     public void setUseLauncher(boolean useLauncher) {
         if (useLauncher) {
             setSearchPath(true);
-            launcher = new Launcher(this);
+            launcher = new ScriptLauncher(this);
         }
     }
 
@@ -365,256 +356,5 @@ public class XhExecBgTask extends XhExecTask implements Runnable, BgProcess {
             }
         }
         
-    }
-}
-
-class Launcher extends Thread {
-    private static String scriptFileName = null;
-
-    private BuildException failure = null;
-    private ServerSocket ssocket = null;
-    private Socket socket = null;
-    private XhExecBgTask task;
-
-    public Launcher(XhExecBgTask t) throws BuildException {
-        super();
-        task = t;
-        prepareScripts();
-        try {
-            InetAddress addr = InetAddress.getByName("127.0.0.1");
-            ssocket = new ServerSocket(0, 0, addr);
-        } catch (Exception e) {
-            if (ssocket != null) {
-                try {
-                    ssocket.close();
-                } catch (Exception ex) {
-                    // ignore
-                }
-                ssocket = null;
-            }
-            String err = "Failed to establish socket for \"" + execName() + "\"";
-            String msg = e.getMessage();
-            if (msg != null) {
-                err = err + ": " + msg;
-            } else {
-                err = err + ": " + e;
-            }
-            task.log(err, Project.MSG_ERR);
-            throw new BuildException(err);
-        }
-    }
-
-    public void run() {
-        // wait for the launcher script to talk to us
-        //
-        try {
-            ssocket.setSoTimeout(30 * 1000); // 30 seconds
-            socket = ssocket.accept();
-            socket.setSoTimeout(3 * 60 * 1000);
-            socket.setTcpNoDelay(true);
-        } catch (SocketTimeoutException stmo) {
-            String msg = "timeout error on initial connection from " + execName();
-            task.log(msg, Project.MSG_ERR);
-            failure = new BuildException(msg);
-        } catch (Exception e) {
-            String err = "error establishing communication with " + execName();
-            String msg = e.getMessage();
-            if (msg != null) {
-                err = err + ": " + msg;
-            } else {
-                err = err + ": " + e;
-            }
-            task.log(err, Project.MSG_ERR);
-            failure = new BuildException(err);
-        }
-    }
-
-    public int getPort() {
-        if (ssocket != null) {
-            return ssocket.getLocalPort();
-        } else {
-            return -1;
-        }
-    }
-
-    // You can call getException() anytime, but for it to be meaningful,
-    // you need to call it after calling start() and then join() on
-    // a Launcher object.
-    //
-    public BuildException getException() {
-        return failure;
-    }
-
-    public void shutdown() throws BuildException {
-        PrintWriter pw = null;
-        BufferedReader in = null;
-        try {
-            pw = new PrintWriter(socket.getOutputStream(), true);
-            pw.println("die");
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            in.readLine();
-        } catch (Exception ex) {
-            // Ignore any exceptions here. Exceptions are typically
-            // the result of race conditions between
-            // async_exec_launcher shutting down due to child process
-            // death and us trying at the same time to tell it to shut
-            // down. It would be nice to be able to distinguish
-            // between actual comm failures that prevent us from
-            // properly interacting with async_exec_launcher and comm
-            // failures due to socket shutdown, but it's really not
-            // possible given the current design and structure of the
-            // code, plus it's not that big of a problem given that
-            // we're limited to localhost loopback communications.
-            //
-        } finally {
-            try {
-                if (pw != null) {
-                    pw.close();
-                }
-            } catch (Exception e) {
-                // ignore
-            }
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (Exception e) {
-                // ignore
-            }
-            try {
-                if (socket != null) {
-                    socket.close();
-                }
-            } catch (Exception e) {
-                // ignore
-            }
-            try {
-                if (ssocket != null) {
-                    ssocket.close();
-                }
-            } catch (Exception e) {
-                // ignore
-            }
-        }
-    }
-
-    public String execName() {
-        return scriptFileName;
-    }
-    
-    private void prepareScripts() {
-        synchronized (getClass()) {
-            if (scriptFileName == null) {
-                File sysTmpDir = new File(System.getProperty("java.io.tmpdir", "."));
-                File launcherTempDir = null;
-                long count = System.currentTimeMillis();
-                do {
-                    launcherTempDir = new File(sysTmpDir, "xharness" + (count++) + ".tmp");
-                } while (launcherTempDir.exists());
-                launcherTempDir.mkdirs();
-                Thread cleanUp = new CleanupThread(launcherTempDir);
-                Runtime.getRuntime().addShutdownHook(cleanUp);
-
-                File scriptFile = null;
-                if (System.getProperty("os.name", "").toLowerCase().startsWith("win")) {
-                    copyResource(task, "bin", "async_exec_launcher_win32.pl", launcherTempDir);
-                    scriptFile = new File(launcherTempDir, "async_exec_launcher.bat");
-
-                    // create launcher batch file
-                    //
-                    try {
-                        OutputStream os = new FileOutputStream(scriptFile);
-                        PrintWriter pw = new PrintWriter(new OutputStreamWriter(os, "US-ASCII"));
-                        pw.println("@echo off");
-                        File f = new File(launcherTempDir, "async_exec_launcher_win32.pl");
-                        pw.print("if not \"%CCPERL%\"==\"\" %CCPERL% ");
-                        pw.print(f.getAbsolutePath());
-                        pw.println(" %*");
-                        pw.print("if \"%CCPERL%\"==\"\" perl.exe ");
-                        pw.print(f.getAbsolutePath());
-                        pw.println(" %*");
-                        pw.close();
-                        os.close();
-                    } catch (IOException ioe) {
-                        task.log("Unable to write batch file "
-                                 + scriptFile.getAbsolutePath() + ": " + ioe,
-                                 Project.MSG_WARN);
-                    }
-                } else {
-                    copyResource(task, "bin", "async_exec_launcher", launcherTempDir);
-                    scriptFile = new File(launcherTempDir, "async_exec_launcher");
-
-                    // make script executable
-                    //
-                    try {
-                        Process proc = Runtime.getRuntime().exec(
-                            new String[] {"chmod", "775", scriptFile.getAbsolutePath()}
-                        );
-                        proc.waitFor();
-                    } catch (Exception ex) {
-                        task.log("Unable to set execute file permission on laucher script "
-                                 + scriptFile.getAbsolutePath() + ": " + ex,
-                                 Project.MSG_WARN);
-                    }
-                }
-
-                try {
-                    scriptFileName = scriptFile.getCanonicalPath();
-                } catch (IOException ioe) {
-                    scriptFileName = scriptFile.getAbsolutePath();
-                }
-            }
-        }
-    }
-    
-    private static void copyResource(Task task, String dir, String file, File toDir) {
-        InputStream is = null;
-        OutputStream os = null;
-        String resourceName = "/" + dir + "/" + file;
-        task.log("Copying resource " + resourceName + " to " + toDir.getAbsolutePath(),
-                Project.MSG_DEBUG);
-        try {
-            is = new BufferedInputStream(
-                    XhExecTask.class.getResourceAsStream(resourceName));
-            os = new BufferedOutputStream(new FileOutputStream(new File(toDir, file)));
-            for (int data = is.read(); data >= 0; data = is.read()) {
-                os.write(data);
-            }
-        } catch (Exception ex) {
-            task.log("Unable to copy resource " + resourceName
-                     + " to " + toDir.getAbsolutePath() + ": "  + ex,
-                     Project.MSG_WARN);
-        } finally {
-            if (is != null) {
-                try { is.close(); } catch (IOException ioe) { /*ignore*/ }
-            }
-            if (os != null) {
-                try { os.close(); } catch (IOException ioe) { /*ignore*/ }
-            }
-        }
-    }
-    
-    private static final class CleanupThread extends Thread {
-        private File dir;
-        
-        public CleanupThread(File f) {
-            dir = f;
-        }
-        
-        public void run() {
-            remove(dir);
-        }
-        
-        private void remove(File f) {
-            if (f.exists()) {
-                if (f.isDirectory()) {
-                    File[] children = f.listFiles();
-                    for (int i = 0; i < children.length; i++) {
-                        remove(children[i]);
-                    }
-                }
-                f.delete();
-            }
-        }
     }
 }
